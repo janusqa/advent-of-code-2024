18de1d28b145a3b7383bee5d1cdfcee153e3f930
Janus Developer <dev@retrievo.net>
2024-12-01 13:13:21 -0400

    Day 1: Historian Hysteria

--- src/Day01/Day01A.php
+++ src/Day01/Day01A.php
@@ -8,6 +8,27 @@ class Day01A
     public function run(string $input): void
     {
         $lines = explode("\n", trim($input));
-        var_dump($lines);
+
+        $left = [];
+        $right = [];
+        $total_distance = 0;
+
+        foreach ($lines as $index => $line) {
+            $parts = preg_split('/\s+/', trim($line));
+            $left[] = (int)$parts[0];
+            $right[] = (int)$parts[1];
+        }
+
+        sort($left);
+        sort($right);
+
+        foreach ($left as $index => $id) {
+            $total_distance += abs($id - $right[$index]);
+        }
+
+        // var_dump($left);
+        // var_dump($right);
+
+        echo ($total_distance . PHP_EOL);
     }
 }
--- src/Day01/Day01B.php
+++ src/Day01/Day01B.php
@@ -8,5 +8,31 @@ class Day01B
     public function run(string $input): void
     {
         $lines = explode("\n", trim($input));
+
+        $left = [];
+        $right = [];
+        $frequency = [];
+        $similarity_score = 0;
+
+        foreach ($lines as $index => $line) {
+            $parts = preg_split('/\s+/', trim($line));
+            $left[] = (int)$parts[0];
+            $right[] = (int)$parts[1];
+
+            if (isset($frequency[$right[$index]])) {
+                $frequency[$right[$index]]++;
+            } else {
+                $frequency[$right[$index]] = 1;
+            }
+        }
+
+        foreach ($left as $id) {
+            $similarity_score += $id * ($frequency[$id] ?? 0);
+        }
+
+        // var_dump($frequency);
+        // var_dump($right);
+
+        echo ($similarity_score . PHP_EOL);
     }
 }
