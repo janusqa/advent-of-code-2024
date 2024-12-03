c6f365ecce3f5059dc12a3817c85a1f9337e5324
Janus Developer <dev@retrievo.net>
2024-12-03 18:10:15 -0400

    Day 3: Mull It Over

--- src/Day03/Day03B.php
+++ src/Day03/Day03B.php
@@ -8,16 +8,25 @@ class Day03B
     public function run(string $input): void
     {
         // $lines = explode("\n", trim($input));
-        $pattern = '/(?:(don\'t\(\)_)?)mul\((\d{1,3}),(\d{1,3})\)/';
+        $pattern_code = '/(don\'t\(\))|(do\(\))|mul\((\d{1,3}),(\d{1,3})\)/';
+        $pattern_instruction = '/mul\((\d{1,3}),(\d{1,3})\)/';
         $result = 0;
 
-        if (preg_match_all($pattern, $input, $matches)) {
-            foreach ($matches[2] as $index => $match) {
-                if (empty($matches[1][$index])) {
-                    $result += intval($match) * intval($matches[3][$index]);
+        if (preg_match_all($pattern_code, $input, $matches)) {
+            $disabled = false;
+            foreach ($matches[0] as $match) {
+                if ($match === "don't()") {
+                    $disabled = true;
+                    continue;
+                } elseif ($match === "do()") {
+                    $disabled = false;
+                    continue;
+                }
+
+                if (!$disabled &&  preg_match($pattern_instruction, $match, $instruction)) {
+                    $result += $instruction[1] * $instruction[2];
                 }
             }
-            // print_r($matches);
         }
 
         echo ($result . PHP_EOL);
