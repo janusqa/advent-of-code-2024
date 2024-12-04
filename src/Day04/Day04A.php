70ccd21a4bf54fe671a62ae722c1cb39be8aa2d9
Janus Developer <dev@retrievo.net>
2024-12-04 17:25:14 -0400

    Day 4 part 1 completed

---
+++ src/Day04/Day04A.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace Janusqa\Adventofcode\Day04;
+
+class Day04A
+{
+
+    public function run(string $input): void
+    {
+        $lines = explode("\n", trim($input));
+
+        $directions = [
+            "nw" => [-1, -1],
+            "n" => [-1, 0],
+            "ne" => [-1, 1],
+            "e" => [0, 1],
+            "se" => [1, 1],
+            "s" => [1, 0],
+            "sw" => [1, -1],
+            "w" => [0, -1],
+        ];
+
+        $word = "XMAS";
+        $word_count = 0;
+
+        $grid = [];
+
+        foreach ($lines as $line) {
+            $grid[] = str_split($line);
+        }
+
+        $RUBound = count($grid) - 1;
+        $CUBound = count($grid[0]) - 1;
+
+        foreach ($grid as $row_index => $row) {
+            foreach ($row as $col_index => $col) {
+                foreach ($directions as $direction) {
+                    $search = [];
+                    $search[] = $col;
+                    $row_temp = $row_index;
+                    $col_temp = $col_index;
+                    for ($i = 0; $i < 3; $i++) {
+                        $row_temp += $direction[0];
+                        $col_temp += $direction[1];
+                        if ($this->OutOfBounds($row_temp, $col_temp, $RUBound, $CUBound)) {
+                            break;
+                        }
+                        $search[] = $grid[$row_temp][$col_temp];
+                    }
+                    if ($word === implode('', $search)) {
+                        $word_count++;
+                    }
+                }
+            }
+        }
+
+        echo ($word_count . PHP_EOL);
+    }
+
+    private function OutOfBounds(int $row, int $col, int $URBound, int $CUBound): bool
+    {
+        return (0 > $row || $row > $URBound || 0 > $col || $col > $CUBound);
+    }
+}
---
+++ src/Day04/Day04B.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Janusqa\Adventofcode\Day04;
+
+class Day04B
+{
+
+    public function run(string $input): void
+    {
+        $lines = explode("\n", trim($input));
+
+
+        echo ("" . PHP_EOL);
+    }
+}
