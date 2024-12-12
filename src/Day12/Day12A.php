84568957b16e14aa2e3a02ada921732d87016dc3
Janus Developer <dev@retrievo.net>
2024-12-12 12:01:38 -0400

    Day 12 part 1 completed

---
+++ src/Day12/Day12A.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace Janusqa\Adventofcode\Day12;
+
+class Day12A
+{
+
+    public function run(string $input): void
+    {
+
+        $lines = explode("\n", trim($input));
+
+        $RUBOUND = count($lines) - 1;
+        $CUBOUND = strlen($lines[0]) - 1;
+
+        $directions = [[-1, 0], [0, 1], [1, 0], [0, -1]];
+
+        $grid = [];
+
+        foreach ($lines as $line) {
+            $grid[] = str_split($line);
+        }
+
+        $visited = [];
+
+        $price = 0;
+
+        foreach ($grid as $ridx => $row) {
+            foreach ($row as $cidx => $plant) {
+                [$area, $perimeter] = $this->survey($grid, $directions, $RUBOUND, $CUBOUND, $visited, $ridx, $cidx, $plant);
+                $price += $area * $perimeter;
+                // if ($area * $perimeter > 0) {
+                //     print_r('Region : ' . $plant . ' => ' . $area . ' * ' . $perimeter . ' = ' . $area * $perimeter . PHP_EOL);
+                // }
+            }
+        }
+
+        echo $price . PHP_EOL;
+    }
+
+    private function survey(array $grid, array $directions, int $RUBOUND, int $CUBOUND, array &$visited, int $ridx, int $cidx, string $plant): array
+    {
+
+        $queue = new \SplQueue();
+
+        $area = 0;
+        $perimeter = 0;
+
+        if (isset($visited["$ridx,$cidx"])) {
+            return [0, 0];
+        }
+
+        $queue->enqueue([$ridx, $cidx]);
+
+        while (!$queue->isEmpty()) {
+
+            [$current_ridx, $current_cidx] = $queue->dequeue();
+
+            if ($grid[$current_ridx][$current_cidx] !== $plant || isset($visited["$current_ridx,$current_cidx"])) {
+                continue;
+            }
+
+            $visited["$current_ridx,$current_cidx"] = true;
+            $area++;
+
+            foreach ($directions as $direction) {
+                $new_ridx = $current_ridx + $direction[0];
+                $new_cidx = $current_cidx + $direction[1];
+
+                if ($this->OutOfBounds($new_ridx, $new_cidx, $RUBOUND, $CUBOUND) || $grid[$new_ridx][$new_cidx] !== $plant) {
+                    $perimeter++;
+                    continue;
+                }
+
+                if (isset($visited["$new_ridx,$new_cidx"])) {
+                    continue;
+                }
+
+                $queue->enqueue([$new_ridx, $new_cidx]);
+            }
+        }
+
+        return [$area, $perimeter];
+    }
+
+    private function OutOfBounds(int $row, int $col, int $RUBOUND, int $CUBOUND): bool
+    {
+        return (0 > $row || $row > $RUBOUND || 0 > $col || $col > $CUBOUND);
+    }
+}
