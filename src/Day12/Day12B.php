f1e93907d4356e908aeff067d1dd0758fef3c637
Janus Developer <dev@retrievo.net>
2024-12-13 03:16:34 -0400

    Day 12: Garden Groups

--- src/Day12/Day12B.php
+++ src/Day12/Day12B.php
@@ -2,7 +2,7 @@
 
 namespace Janusqa\Adventofcode\Day12;
 
-class Day12A
+class Day12B
 {
 
     public function run(string $input): void
@@ -27,8 +27,11 @@ class Day12A
 
         foreach ($grid as $ridx => $row) {
             foreach ($row as $cidx => $plant) {
-                [$area, $perimeter] = $this->survey($grid, $directions, $RUBOUND, $CUBOUND, $visited, $ridx, $cidx, $plant);
-                $price += $area * $perimeter;
+                [$area, $sides] = $this->survey($grid, $directions, $RUBOUND, $CUBOUND, $visited, $ridx, $cidx, $plant);
+                $price += $area * $sides;
+                // if ($area > 0) {
+                //     print_r("A region of $plant plants with price $area * $sides = " . $area * $sides . '.' . PHP_EOL);
+                // }
             }
         }
 
@@ -37,6 +40,7 @@ class Day12A
 
     private function survey(array $grid, array $directions, int $RUBOUND, int $CUBOUND, array &$visited, int $ridx, int $cidx, string $plant): array
     {
+
         if (isset($visited["$ridx,$cidx"])) {
             return [0, 0];
         }
@@ -44,7 +48,7 @@ class Day12A
         $queue = new \SplQueue();
 
         $area = 0;
-        $perimeter = 0;
+        $perimeters = [];
 
         $queue->enqueue([$ridx, $cidx]);
 
@@ -64,7 +68,7 @@ class Day12A
                 $new_cidx = $current_cidx + $direction[1];
 
                 if ($this->OutOfBounds($new_ridx, $new_cidx, $RUBOUND, $CUBOUND) || $grid[$new_ridx][$new_cidx] !== $plant) {
-                    $perimeter++;
+                    $perimeters[] = "$current_ridx,$current_cidx,$new_ridx,$new_cidx";
                     continue;
                 }
 
@@ -76,7 +80,40 @@ class Day12A
             }
         }
 
-        return [$area, $perimeter];
+        return [$area, $this->sides($directions, $perimeters)];
+    }
+
+    private function sides(array $directions, array $perimeters): int
+    {
+        $visited = [];
+        $sides = 0;
+
+        foreach ($perimeters as $perimeter) {
+            if (isset($visited[$perimeter])) {
+                continue;
+            }
+
+            $sides++;
+
+            [$p0, $p1, $p2, $p3] = explode(",", $perimeter);
+
+            foreach ($directions as $direction) {
+                $new_p0 = $p0 + $direction[0];
+                $new_p1 = $p1 + $direction[1];
+                $new_p2 = $p2 + $direction[0];
+                $new_p3 = $p3 + $direction[1];
+
+                while (in_array("$new_p0,$new_p1,$new_p2,$new_p3", $perimeters)) {
+                    $visited["$new_p0,$new_p1,$new_p2,$new_p3"] = true;
+                    $new_p0 += $direction[0];
+                    $new_p1 += $direction[1];
+                    $new_p2 += $direction[0];
+                    $new_p3 += $direction[1];
+                }
+            }
+        }
+
+        return $sides;
     }
 
     private function OutOfBounds(int $row, int $col, int $RUBOUND, int $CUBOUND): bool
