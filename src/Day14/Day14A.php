996b99edc86854ec4545fe771c42be6aa1299809
Janus Developer <dev@retrievo.net>
2024-12-15 03:43:38 -0400

    Day 14: Restroom Redoubt

--- src/Day14/Day14A.php
+++ src/Day14/Day14A.php
@@ -19,28 +19,26 @@ class Day14A
             $robots[] = array_map(fn($n) => (int)$n, $matches[0]);
         }
 
-        for ($i = 0; $i < count($robots); $i++) {
-            $robots[$i][0] = $this->wrap($robots[$i][0] + $robots[$i][2] * 100, $CUBOUND);
-            $robots[$i][1] = $this->wrap($robots[$i][1] + $robots[$i][3] * 100, $RUBOUND);
-        }
-
-        $xm = floor($CUBOUND / 2);
-        $ym = floor($RUBOUND / 2);
-
-        $safety_factor = 1;
+        $xm = (int)floor($CUBOUND / 2);
+        $ym = (int)floor($RUBOUND / 2);
         $quadrants = [];
 
-        foreach ($robots as $robot) {
-            if ($this->InBounds($robot[1], $robot[0], 0, 0, $ym - 1, $xm - 1)) {
+        for ($i = 0; $i < count($robots); $i++) {
+            $robots[$i][0] = $this->wrap($robots[$i][0] + ($robots[$i][2] * 100), $CUBOUND);
+            $robots[$i][1] = $this->wrap($robots[$i][1] + ($robots[$i][3] * 100), $RUBOUND);
+
+            if ($this->InBounds($robots[$i][1], $robots[$i][0], 0, 0, $ym - 1, $xm - 1)) {
                 $quadrants['q1'] = ($quadrants['q1'] ?? 0) + 1;
-            } elseif ($this->InBounds($robot[1], $robot[0], 0, $xm + 1, $ym - 1, $CUBOUND - 1)) {
+            } elseif ($this->InBounds($robots[$i][1], $robots[$i][0], 0, $xm + 1, $ym - 1, $CUBOUND - 1)) {
                 $quadrants['q2'] = ($quadrants['q2'] ?? 0) + 1;
-            } elseif ($this->InBounds($robot[1], $robot[0], $ym + 1, 0, $RUBOUND - 1, $xm - 1)) {
+            } elseif ($this->InBounds($robots[$i][1], $robots[$i][0], $ym + 1, 0, $RUBOUND - 1, $xm - 1)) {
                 $quadrants['q3'] = ($quadrants['q3'] ?? 0) + 1;
-            } elseif ($this->InBounds($robot[1], $robot[0], $ym + 1, $xm + 1, $RUBOUND - 1, $CUBOUND - 1)) {
+            } elseif ($this->InBounds($robots[$i][1], $robots[$i][0], $ym + 1, $xm + 1, $RUBOUND - 1, $CUBOUND - 1)) {
                 $quadrants['q4'] = ($quadrants['q4'] ?? 0) + 1;
             }
         }
+
+        $safety_factor = 1;
 
         foreach ($quadrants as $quadrant) {
             $safety_factor *= $quadrant;
--- src/Day14/Day14B.php
+++ src/Day14/Day14B.php
@@ -4,59 +4,92 @@ namespace Janusqa\Adventofcode\Day14;
 
 class Day14B
 {
+    private $file = "./output.txt";
 
     public function run(string $input): void
     {
         $lines = explode("\n", trim($input));
 
-        $RUBOUND = 7; //103;
-        $CUBOUND = 11; //101;
+        $RUBOUND = 103;
+        $CUBOUND = 101;
         $pattern_robot = '/-?\d+/';
         $robots = [];
-        // $grid = [];
 
         foreach ($lines as $line) {
             preg_match_all($pattern_robot, $line, $matches);
             $robots[] = array_map(fn($n) => (int)$n, $matches[0]);
         }
 
-        for ($i = 0; $i < count($robots); $i++) {
-            $robots[$i][0] = $this->wrap($robots[$i][0] + $robots[$i][2] * 100, $CUBOUND);
-            $robots[$i][1] = $this->wrap($robots[$i][1] + $robots[$i][3] * 100, $RUBOUND);
-            // $grid[$robots[$i][1]][$robots[$i][0]] = ($grid[$robots[$i][1]][$robots[$i][0]] ?? 0) + 1;
+        $xm = (int)floor($CUBOUND / 2);
+        $ym = (int)floor($RUBOUND / 2);
+
+        $seconds = 0;
+        $seen = [];
+
+        while (true) {
+            $grid = [];
+            // $quadrants = [];
+
+            for ($i = 0; $i < count($robots); $i++) {
+                $next_x = $this->wrap($robots[$i][0] + ($robots[$i][2] * $seconds), $CUBOUND);
+                $next_y = $this->wrap($robots[$i][1] + ($robots[$i][3] * $seconds), $RUBOUND);
+
+                // if ($this->InBounds($robots[$i][1], $robots[$i][0], 0, 0, $ym - 1, $xm - 1)) {
+                //     $quadrants['q1'] = ($quadrants['q1'] ?? 0) + 1;
+                // } elseif ($this->InBounds($robots[$i][1], $robots[$i][0], 0, $xm + 1, $ym - 1, $CUBOUND - 1)) {
+                //     $quadrants['q2'] = ($quadrants['q2'] ?? 0) + 1;
+                // } elseif ($this->InBounds($robots[$i][1], $robots[$i][0], $ym + 1, 0, $RUBOUND - 1, $xm - 1)) {
+                //     $quadrants['q3'] = ($quadrants['q3'] ?? 0) + 1;
+                // } elseif ($this->InBounds($robots[$i][1], $robots[$i][0], $ym + 1, $xm + 1, $RUBOUND - 1, $CUBOUND - 1)) {
+                //     $quadrants['q4'] = ($quadrants['q4'] ?? 0) + 1;
+                // }
+
+                $grid[$next_y][$next_x] = ($grid[$next_y][$next_x] ?? 0) + 1;
+            }
+
+            $grid_key  = serialize($grid);
+            if (isset($seen[$grid_key])) {
+                echo "Robot rotation repetition detected at $seen[$grid_key]";
+                break;
+            };
+            $seen[$grid_key] = $seconds;
+
+            $this->printGrid($grid, $RUBOUND, $CUBOUND, $seconds);
+
+            // $safety_factor = 1;
+            // foreach ($quadrants as $quadrant) {
+            //     $safety_factor *= $quadrant;
+            // }
+
+            $seconds++;
         }
 
-        $xm = floor($CUBOUND / 2);
-        $ym = floor($RUBOUND / 2);
-
-        $safety_factor = 0;
-        $quadrants = [];
-
-        foreach ($robots as $robot) {
-            if ($robot)
-                $safety_factor += $robot[0] === $xm || $robot[1] === $ym ? 0 : 1;
-        }
-
-        // $this->printGrid($grid, $RUBOUND, $CUBOUND);
-
-        echo $safety_factor . PHP_EOL;
+        // echo $seconds . PHP_EOL;
     }
-
 
     private function wrap(int $index, int $wrapAt): int
     {
         return ($index % $wrapAt + $wrapAt) % $wrapAt;
     }
 
-
-
-    // private function printGrid(array $grid, int $rows, int $cols): void
+    // private function InBounds(int $row, int $col, int $r_start, int $c_start, int $r_end, int $c_end): bool
     // {
-    //     for ($r = 0; $r < $rows; $r++) {
-    //         for ($c = 0; $c < $cols; $c++) {
-    //             echo isset($grid[$r][$c]) ? $grid[$r][$c] : '.'; // Print grid count or '.' if empty
-    //         }
-    //         echo PHP_EOL;
-    //     }
+    //     return ($r_start <= $row && $row <= $r_end && $c_start <= $col && $col <= $c_end);
     // }
+
+    private function printGrid(array $grid, int $rows, int $cols, $seconds = 0): void
+    {
+        file_put_contents($this->file, "Seconds: $seconds" . PHP_EOL, FILE_APPEND);
+        // echo "Seconds: $seconds" .PHP_EOL;
+        for ($r = 0; $r < $rows; $r++) {
+            for ($c = 0; $c < $cols; $c++) {
+                file_put_contents($this->file, isset($grid[$r][$c]) ? $grid[$r][$c] : '.', FILE_APPEND); // Print grid count or '.' if empty
+                // echo isset($grid[$r][$c]) ? $grid[$r][$c] : '.'; // Print grid count or '.' if empty
+            }
+            file_put_contents($this->file, PHP_EOL, FILE_APPEND);
+            // echo PHP_EOL;
+        }
+        file_put_contents($this->file, PHP_EOL . PHP_EOL, FILE_APPEND);
+        // echo PHP_EOL . PHP_EOL;
+    }
 }
